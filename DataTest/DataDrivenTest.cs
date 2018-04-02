d97539e7b9199d72767347dddcd7931c1c482f9e
Natalia Damorad <NataliaDamorad@coherentsolutions.com>
2018-04-02 18:32:55 +0300

    Added new files

---
+++ DataTest/DataDrivenTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using System.Data;
+
+namespace SeleniumTask1.DataTest
+{
+    /// <summary>
+    /// Summary description for DataDrivenTest
+    /// Task 40: 5.	Create DDT test, which will cover login functionality (only happy path with > 3 datasets).
+    /// </summary>
+    [TestClass]
+    public class DataDrivenTest
+    {
+        IWebDriver driver = new ChromeDriver();
+
+
+        [TestMethod]
+        [DeploymentItem("Data.xlsx")]
+        [DataSource("System.Data.OleDb", "Provider=Microsoft.         ACE.OLEDB.12.0;Data Source=Data.xls;Persist Security          Info=False;Extended Properties='Excel 12.0;HDR=Yes'", "Data$", DataAccessMethod.Sequential)]
+        public void DataDrivenTestMethod()
+        {
+            driver.Navigate().GoToUrl("https://192.168.100.26/");
+            IWebElement Username = driver.FindElement(By.Id("Username"));
+            Username.SendKeys((TestContext.DataRow["Username"]).ToString());
+
+            IWebElement Password = driver.FindElement(By.Id("Password"));
+            Password.SendKeys((TestContext.DataRow["Password"]).ToString());
+
+            driver.FindElement(By.Id("SubmitButton")).Click();
+            Assert.AreEqual("RMSys - Home", driver.Title.ToString());
+            TestContext.WriteLine("The user " + (TestContext.DataRow["Username"]).ToString() +  " was sign succesfully");
+        }
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+    }
+}
---
+++ LogoutTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumTask1
+{
+    /// <summary>
+    /// 2. Create test 2, which perform logout from RMSys.
+    /// 3. Modify test 2: add extra assertion, which will be assert, that current URL contains the following part – “192.168.100.26”.
+    /// </summary>
+    [TestClass]
+    public class LogoutClass
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [TestMethod]
+        public void LogoutTest()
+        {
+            driver.Navigate().GoToUrl("https://192.168.100.26/");
+            driver.FindElement(By.Id("Username")).SendKeys("EugenBorisik");
+            driver.FindElement(By.Id("Password")).SendKeys("qwerty12345");
+            driver.FindElement(By.Id("SubmitButton")).Click();
+            //Verify if current URL contains the following part – “192.168.100.26”.
+            //Assert.IsTrue(driver.Url.ToString().Contains("192.168.100.26"));
+            driver.FindElement(By.LinkText("Sign Out")).Click();
+            Assert.AreEqual("RMSys - Sign In", driver.Title.ToString());
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
