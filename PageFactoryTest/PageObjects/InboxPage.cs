2afa7e7637d9235d7f0259feecfc29a63f6b4dae
Natalia Damorad <NataliaDamorad@coherentsolutions.com>
2018-05-30 13:06:17 +0300

    Update Page Factory files

---
+++ PageFactoryTest/PageObjects/InboxPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+//using OpenQA.Selenium.Support.PageObjects;
+using PageFactory = SeleniumExtras.PageObjects.PageFactory;
+
+namespace SeleniumTask1.PageFactoryTest.PageObjects
+{
+    public class InboxPage
+    {
+        private IWebDriver driver;
+
+        public InboxPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        //[FindsBy(How = How.CssSelector, Using = "a[title='Sign out']")] //- doesn't work more
+        //public IWebElement LogoutLink { get; set; }
+        public IWebElement LogoutLink => driver.FindElement(By.CssSelector("a[title='Sign out']"));
+    }
+}
---
+++ PageFactoryTest/TestCases/LoginTest.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeleniumTask1.PageFactoryTest.PageObjects;
+
+
+namespace SeleniumTask1.PageFactoryTest.TestCases
+{
+    /// <summary>
+    /// Task 60: 1.	Automation project should use Page Factory pattern (Selenium).
+    /// </summary>
+    [TestClass]
+    public class LoginTest
+    {
+        private IWebDriver driver = new ChromeDriver();
+
+        [TestMethod, TestCategory("PageFactory")]
+        public void LoginTestMethod()
+        {
+            var loginPage = new LoginPage(driver);
+            loginPage.GetUrl(driver);
+            loginPage.LoginAccount("nataliadamorad", "Vintage2018");
+            Assert.AreEqual("RMSys - Home", driver.Title.ToString());
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
